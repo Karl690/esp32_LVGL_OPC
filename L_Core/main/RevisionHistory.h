992c97ce7b5360eca6dbefd917ae0db550695b29
falcon developer <falcon198236@outlook.com>
2023-06-22 11:07:22 +0200

    6/21/2023 Added RevisionHistory

---
+++ L_Core/main/RevisionHistory.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define RevisionHeader ">Hy:L_Core:"
+#define MajorStep      "1.000"
+#define RevisionDate   "6/21/2023"
+#define CONNECTIONSTRING ">Hy:L_Core Rev " MajorStep ":" RevisionDate ":ESP32SC01 Plus:---:"   //"1.000:0:0:0:USB"
--- L_Core/main/wifi/wifi.h
+++ L_Core/main/wifi/wifi.h
@@ -1,7 +1,7 @@
 #include "esp_wifi.h"
 
-#define WIFI_SSID "Chaoyou2"
-#define WIFI_PASSWORD "1234567890"
+#define WIFI_SSID "SSID"
+#define WIFI_PASSWORD "PASSWORD"
 
 extern esp_netif_ip_info_t wifi_info;
 extern bool wifi_is_connected;
